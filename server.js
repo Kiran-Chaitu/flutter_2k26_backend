afebfc999075a29e4c938a1fb8971cb0033ef9a6
Kiran-Chaitu <22A91A05D0@aec.edu.in>
2026-05-12 09:53:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const students = require("./students");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Flutter Login API Running");
+});
+
+app.post("/login", (req, res) => {
+  try {
+    const { usn, password } = req.body;
+
+    if (!usn || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all fields"
+      });
+    }
+
+    const student = students.find(
+      (item) => item.usn.toLowerCase() === usn.toLowerCase()
+    );
+
+    if (!student) {
+      return res.status(404).json({
+        success: false,
+        message: "Student not found"
+      });
+    }
+
+    if (password !== "Torii@123") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: student
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ students.js
@@ -0,0 +1,24 @@
+const students = [
+  {
+    usn: "1NC23CI003",
+    name: "ADITHYA RAJ M",
+    branch: "AIML"
+  },
+  {
+    usn: "1NC23CI004",
+    name: "AKASH A S",
+    branch: "AIML"
+  },
+  {
+    usn: "1NC23CI005",
+    name: "ANVITA R N",
+    branch: "AIML"
+  },
+  {
+    usn: "1NC23CI006",
+    name: "ARCHITHA T",
+    branch: "AIML"
+  }
+];
+
+module.exports = students;
